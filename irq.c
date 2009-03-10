6e5333ec900ba6e90d2f67e96dcad6299aa0b5f6
gke3500 <gke3500@6150ef54-a058-11dd-a159-232c7ab52622>
2009-03-10 02:27:27 +0000

    Refresh

--- irq.c
+++ irq.c
@@ -1,27 +1,25 @@
-// ==============================================
-// =      U.A.V.P Brushless UFO Controller      =
-// =           Professional Version             =
-// = Copyright (c) 2007 Ing. Wolfgang Mahringer =
-// =      Rewritten 2008 Ing. Greg Egan         =
-// ==============================================
+// =======================================================================
+// =                   U.A.V.P Brushless UFO Controller                  =
+// =                         Professional Version                        =
+// =             Copyright (c) 2007 Ing. Wolfgang Mahringer              =
+// =           Extensively modified 2008-9 by Prof. Greg Egan            =
+// =                          http://www.uavp.org                        =
+// =======================================================================
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation; either version 2 of the License, or
 //  (at your option) any later version.
-//
+
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//
+
 //  You should have received a copy of the GNU General Public License along
 //  with this program; if not, write to the Free Software Foundation, Inc.,
 //  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-//
-// ==============================================
-// =  please visit http://www.uavp.org          =
-// ==============================================
+
 
 // Interrupt routine
 // Major changes to irq.c including removal of redundant source by Ing. Greg Egan - 
@@ -30,52 +28,46 @@
 #include "pu-test.h"
 #include "bits.h"
 
-#include <int16cxx.h>	//interrupt support
-
-#pragma origin 4
-
 // Interrupt Routine
 
-bank1 int16 	NewK1, NewK2, NewK3, NewK4, NewK5, NewK6, NewK7;
-
+#pragma udata irqvars
+int16 	NewK1, NewK2, NewK3, NewK4, NewK5, NewK6, NewK7;
 uns8	RecFlags;
+#pragma udata
 
-#pragma interruptSaveCheck w
-
-#define USE_FILTERS
-
-interrupt irq(void)
+#pragma interrupt low_isr_handler
+void low_isr_handler(void)
 {
-int8	NewRoll, NewNick, NewTurn;	
-int16 	Temp;
-uns16 	CCPR1 @0x15;
+	return;
+} // low_isr_handler
 
-// For 2.4GHz systems see README_DSM2_ETC.
- 
-	int_save_registers;	// save W and STATUS
+#pragma interrupt high_isr_handler
+void high_isr_handler(void)
+{
+	int8	NewRoll, NewPitch, NewYaw;	
+	int16 	Temp;
 
-	if( T0IF && T0IE )
+	// For 2.4GHz systems see README_DSM2_ETC.
+	if( INTCONbits.TMR0IF && INTCONbits.TMR0IE )
 	{
-		T0IF = 0;				// quit int
+		INTCONbits.TMR0IF = 0;				// quit int
 		TimeSlot--;
 	}
 
-	if( CCP1IF )
+	if( PIR1bits.CCP1IF )
 	{
 		TMR2 = 0;				// re-set timer and postscaler
-		TMR2IF = 0;				// quit int
+		PIR1bits.TMR2IF = 0;				// quit int
 		_FirstTimeout = 0;
 
-#ifndef RX_PPM						// single PPM pulse train from receiver
+		#ifndef RX_PPM		// single PPM pulse train from receiver
 							// standard usage (PPM, 3 or 4 channels input)
-		CCPR1.low8 = CCPR1L;
-		CCPR1.high8 = CCPR1H;
-		CCP1M0 ^= 1;	// toggle edge bit
+		CCP1CONbits.CCP1M0 ^= 1;	// toggle edge bit
 		PR2 = TMR2_5MS;				// set compare reg to 5ms
 
-		if( NegativePPM ^ CCP1M0  )		// a negative edge
+		if( NegativePPM ^ CCP1CONbits.CCP1M0  )		// a negative edge
 		{
-#endif
+		#endif
 			// could be replaced by a switch ???
 
 			if( RecFlags == 0 )
@@ -103,15 +95,15 @@ uns16 	CCPR1 @0x15;
 				NewK6 = NewK7 - NewK6;
 				CurrK6 = NewK6 >> 1;
 			}
-#ifdef RX_PPM
+		#ifdef RX_PPM
 			else
-#else
+		#else
 			else	// values are unsafe
 				goto ErrorRestart;
 		}
 		else	// a positive edge
 		{
-#endif // RX_PPM 
+		#endif // RX_PPM 
 			if( RecFlags == 1 )
 			{
 				NewK2 = CCPR1;
@@ -137,13 +129,13 @@ uns16 	CCPR1 @0x15;
 				CurrK4 = NewK4;
 				CurrK5 = NewK5;
 				_NewValues = 1;
-// sanity check
-// NewKx has values in 4us units now. content must be 256..511 (1024-2047us)
-				if( (NewK1.high8 == 1) &&
-				    (NewK2.high8 == 1) &&
-				    (NewK3.high8 == 1) &&
-				    (NewK4.high8 == 1) &&
-				    (NewK5.high8 == 1) )
+				// sanity check
+				// NewKx has values in 4us units now. content must be 256..511 (1024-2047us)
+				if( ((NewK1>>8) == 1) &&
+				    ((NewK2>>8) == 1) &&
+				    ((NewK3>>8) == 1) &&
+				    ((NewK4>>8) == 1) &&
+				    ((NewK5>>8) == 1) )
 				{
 					_NoSignal = 0;
 				}
@@ -163,24 +155,24 @@ ErrorRestart:
 				_NewValues = 0;
 				_NoSignal = 1;		// Signal lost
 				RecFlags = -1;
-#ifndef RX_PPM
+				#ifndef RX_PPM
 				if( NegativePPM )
-					CCP1M0 = 1;	// wait for positive edge next
+					CCP1CONbits.CCP1M0 = 1;	// wait for positive edge next
 				else
-					CCP1M0 = 0;	// wait for negative edge next
-#endif
+					CCP1CONbits.CCP1M0 = 0;	// wait for negative edge next
+				#endif
 			}	
-#ifndef RX_PPM
+		#ifndef RX_PPM
 		}
-#endif
-		CCP1IF = 0;				// quit int
+		#endif
+		PIR1bits.CCP1IF = 0;				// quit int
 		RecFlags++;
 	}
 
-	if( TMR2IF )	// 5 or 14 ms have elapsed without an active edge
+	if( PIR1bits.TMR2IF )	// 5 or 14 ms have elapsed without an active edge
 	{
-		TMR2IF = 0;	// quit int
-#ifndef RX_PPM	// single PPM pulse train from receiver
+		PIR1bits.TMR2IF = 0;	// quit int
+		#ifndef RX_PPM	// single PPM pulse train from receiver
 		if( _FirstTimeout )			// 5 ms have been gone by...
 		{
 			PR2 = TMR2_5MS;			// set compare reg to 5ms
@@ -188,11 +180,23 @@ ErrorRestart:
 		}
 		_FirstTimeout = 1;
 		PR2 = TMR2_14MS;			// set compare reg to 14ms
-#endif
+		#endif
 		RecFlags = 0;
 	}
-
 	
-	int_restore_registers;
-}
+} // high_isr_handler
+	
+#pragma code high_isr = 0x08
+void high_isr (void)
+{
+  _asm goto high_isr_handler _endasm
+} // high_isr
+#pragma code
+
+#pragma code low_isr = 0x18
+void low_isr (void)
+{
+  _asm goto low_isr_handler _endasm
+} // low_isr
+#pragma code
 
--- pu-test.h
+++ pu-test.h
@@ -1,31 +1,28 @@
-#ifndef BATCHMODE
-// ==============================================
-// =    U.A.V.P Brushless UFO Test-Software     =
-// =           Professional Version             =
-// = Copyright (c) 2007 Ing. Wolfgang Mahringer =
-// ==============================================
+
+// =======================================================================
+// =                   U.A.V.P Brushless UFO Controller                  =
+// =                         Professional Version                        =
+// =             Copyright (c) 2007 Ing. Wolfgang Mahringer              =
+// =           Extensively modified 2008-9 by Prof. Greg Egan            =
+// =                          http://www.uavp.org                        =
+// =======================================================================
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation; either version 2 of the License, or
 //  (at your option) any later version.
-//
+
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//
+
 //  You should have received a copy of the GNU General Public License along
 //  with this program; if not, write to the Free Software Foundation, Inc.,
 //  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-//
-// ==============================================
-// =  please visit http://www.uavp.de           =
-// =               http://www.mahringer.co.at   =
-// ==============================================
 
 // C-Ufo Header File
-
+#ifndef BATCHMODE
 // ==============================================
 // == Global compiler switches
 // ==============================================
@@ -33,144 +30,120 @@
 // use this to allow debugging with MPLAB's Simulator
 // #define SIMU
 //
-// select your board version here
-// DO NOT FORGET TO SET THE SAME DEFINE IN BOTLOADER.ASM!!!
-//
 // Only one of the following 2 defines must be activated:
 // When using 3 ADXRS300 or -150 gyros
-#define OPT_ADXRS
+//#define OPT_ADXRS
 // When using 1 ADXRS300 and 1 IDG300 gyro
-//#define OPT_IDG
+#define OPT_IDG
 
-//
+// defined: serial PPM pulse train from receiver
+// undefined: standard servo pulses from CH1, 3, 5 and 7
+//#define RX_PPM
+
 // Select what speeed controllers to use
 // to use standard PPM pulse
 #define ESC_PPM
-// to use X3D BL controllers (courtesy to Jast :-) not yet tested!)
-// #define ESC_X3D
+
 // to use Holgers ESCs (tested by ufo-hans)
 //#define ESC_HOLGER
-// to use YGE I2C controllers (for standard YGEs use ESC_PPM)
-//#define ESC_YGEI2C
 
 #endif // !BATCHMODE
-
 
 // Board version
 #define Version	"3.14m3"
 
+#include <p18cxxx.h> 
+#include <math.h>
+#include <delays.h>
+#include <timers.h>
+#include <usart.h>
+#include <capture.h>
+#include <adc.h>
+
+// Types
+
+typedef unsigned char uns8 ;
+typedef unsigned char uint8 ;
+typedef signed char int8;
+typedef unsigned int uns16;
+typedef unsigned int uint16;
+typedef int int16;
+typedef long int32;
+typedef unsigned long uint32;
+typedef short long int24;
+typedef unsigned short long uint24;
+
+#define IsSet(S,b) 		((uns8)((S>>b)&1))
+#define Invert(S,b) 	((uns8)(S^=(1<<b)))
+
+#define DisableInterrupts (INTCONbits.GIEH=0)
+#define EnableInterrupts (INTCONbits.GIEH=1)
+
+// ADC Channels
+#define ADCPORTCONFIG 0b00001010 // AAAAA
+#define ADCBattVoltsChan 	0 
+
+#ifdef OPT_ADXRS
+	#define ADCRollChan 	1
+	#define ADCPitchChan 	2
+#else // OPT_IDG
+	#define ADCRollChan 	2
+	#define ADCPitchChan 	1
+#endif // OPT_ADXRS
+
+#define ADCVRefChan 			3 
+#define ADCYawChan			4
+
+#define ADCVREF5V 			0
 
 // ==============================================
 // == External variables
 // ==============================================
 
-
-extern	shrBank	uns8	Flags;
-extern	shrBank	uns8	RecFlags;	// Interrupt save registers for FSR
-
-extern	shrBank	uns8	LedShadow;	// shadow register
-
-extern	shrBank	uns8	ConfigReg;
-bit NegativePPM		@ConfigReg.0;
-
+extern	 uns8	Flags[8] , Flags2[8];
+extern	 uns8	RecFlags;	// Interrupt save registers for FSR
+extern	 uns8	LedShadow;	// shadow register
+extern	 uns8	ConfigReg;
 // the CurrK1..CurrK7 variables MUST be in this order!
-extern	bank0	uns16	CurrK1,CurrK2,CurrK3,CurrK4;
-extern	bank0	uns16	CurrK5,CurrK6,CurrK7;
-extern	bank1	uns16	PauseTime; 
-extern  bank0	uns8	BaroType, BaroTemp;
-extern	bank1	uns8	MVorne, MHinten, MLinks, MRechts;
-extern	bank1	uns8	MCamRoll, MCamNick;
-extern	bank1	uns8	EscI2CFlags;
-extern	bank1 	uns16 	BaroTemperature, BaroPressure;
-
+extern	 uns16	CurrK1,CurrK2,CurrK3,CurrK4;
+extern	 uns16	CurrK5,CurrK6,CurrK7;
+extern	 uns16	PauseTime; 
+extern	 uns8	EscI2CFlags;
 // special locations
-extern	bank1	uns16	nilgval;
+extern	 int24	nilgval;
+extern	 int8 	TimeSlot;
+extern	 uns8	mSTick;
 
 // EEPROM parameter set addresses
 
 #define _EESet1	0		// first set starts at address 0x00
 #define _EESet2	0x20	// second set starts at address 0x20
 
-// Die Reihenfolge dieser Variablen MUSS gewahrt bleiben!!!!
-// These variables MUST keep their order!!!
-
-extern	bank1	int8	RollPropFactor; 	// 01
-extern	bank1	int8	RollIntFactor;		// 02
-extern	bank1	int8	RollDiffFactor;		// 03
-extern	bank1	int8 RollLimit;			// 04
-extern	bank1	int8	RollIntLimit;		// 05
-extern	BaroTempCoeff @RollLimit;
-
-extern	bank1	int8	PitchPropFactor;	 	// 06
-extern	bank1	int8	PitchIntFactor;		// 07
-extern	bank1	int8	PitchDiffFactor;		// 08
-extern	bank1	int8 	PitchLimit;			// 09
-extern	bank1	int8	PitchIntLimit;		// 10
-extern  BaroThrottleProp @PitchLimit;
-
-extern	bank1	int8	YawPropFactor; 	// 11
-extern	bank1	int8	YawIntFactor;		// 12
-extern	bank1	int8	YawDiffFactor;		// 13
-extern	bank1	int8	YawLimit;			// 14
-extern	bank1	int8 	YawIntLimit;		// 15
-
-extern	bank1	int8	ConfigParam;		// 16
-extern	bank1	int8 	TimeSlot;			// 17
-extern	bank1	int8	LowVoltThres;		// 18
-
-extern	bank1	int8	LinLRIntFactor;		// 19 free
-extern	bank1	int8	LinFBIntFactor;		// 20 free
-extern	bank1	int8	LinUDIntFactor;		// 21
-extern	bank1	int8 	MiddleUD;			// 22
-extern	bank1	int8	MotorLowRun;		// 23
-extern	bank1	int8	MiddleLR;			// 24
-extern	bank1	int8	MiddleFB;			// 25
-extern	bank1	int8	CamPitchFactor;		// 26
-extern	CamRollFactor @LinLRIntFactor;
-extern	bank1	int8	CompassFactor;		// 27
-extern	bank1	int8	BaroThrottleDiff;	// 28
-
-// these 2 dummy registers (they do not occupy any RAM location)
-// are here for defining the first and the last programmable 
-// register in a set
-
-int FirstProgReg @RollPropFactor;
-int	LastProgReg @BaroThrottleDiff;
-
-#define DELAY_MS(ms)	for( W=ms; W!=0; W--){T0IF=0;while(T0IF == 0);}
+#define DELAY_MS(ms) for( mSTick=ms; mSTick!=0; mSTick--){INTCONbits.T0IF=0;while(INTCONbits.T0IF == 0);}
 
 #define _ClkOut		(160/4)	/* 16.0 MHz quartz */
-#define _PreScale0	16	/* 1:16 TMR0 prescaler */
-#define _PreScale1	8	/* 1:8 TMR1 prescaler */
-#define _PreScale2	16
-#define _PostScale2	16
+#define TMR2_5MS	78	/* 1x 5ms +  */
+#define TMR2_14MS	234	/* 1x 15ms = 20ms pause time */
 
-// wegen dem dummen Compiler muss man händisch rechnen :-(
-#define TMR2_5MS	78	/* 1x 4ms +  */
-#define TMR2_14MS	219	/* 1x 15ms = 20ms pause time */
-
-//                    RX impuls times in 10-microseconds units
-//                    vvv   ACHTUNG: Auf numerischen Überlauf achten!
+// RX impuls times in 10-microseconds units
 #ifdef ESC_PPM
-#define	_Minimum	((105* _ClkOut/(2*_PreScale1))&0xFF)	/*-100% */
+#define	_Minimum	1
+#define _Maximum	240	
 #endif
 #ifdef ESC_X3D
 #define _Minimum	0
+#define _Maximum	255
 #endif
 #ifdef ESC_HOLGER
 #define _Minimum	0
+#define _Maximum	225	/* ESC demanded */
 #endif
 #ifdef ESC_YGEI2C
 #define _Minimum	0
+#define _Maximum	240	/* ESC demanded */
 #endif
 
-#define _Neutral	((150* _ClkOut/(2*_PreScale1))&0xFF)    /*   0% */
-#define _Maximum	((195* _ClkOut/(2*_PreScale1))&0xFF)	/*+100% */
-#define _ThresStop	((113* _ClkOut/(2*_PreScale1))&0xFF)	/*-90% ab hier Stopp! */
-#define _ThresStart	((116* _ClkOut/(2*_PreScale1))&0xFF)	/*-85% ab hier Start! */
-#define _ProgMode	((160* _ClkOut/(2*_PreScale1))&0xFF)	/*+75% */
-#define _ProgUp		((150* _ClkOut/(2*_PreScale1))&0xFF)	/*+60% */
-#define _ProgDown	((130* _ClkOut/(2*_PreScale1))&0xFF)	/*-60% */
+#define _Neutral	((_Maximum+_Minimum+1)/2)
 
 // ==============================================
 // == Sanity checks of the #defines
@@ -179,9 +152,7 @@ int	LastProgReg @BaroThrottleDiff;
 #if _Minimum >= _Maximum
 #error _Minimum < _Maximum!
 #endif
-#if _ThresStart <= _ThresStop
-#error _ThresStart <= _ThresStop!
-#endif
+
 #if (_Maximum < _Neutral)
 #error Maximum < _Neutral !
 #endif
@@ -199,128 +170,115 @@ int	LastProgReg @BaroThrottleDiff;
 #define _B19200	(_ClkOut*100000/(4*19200) - 1)
 #define _B38400	(_ClkOut*100000/(4*38400) - 1)
 
-
-// Parameters for SendComText()
-#define _SerHello	0
-#define _SerSetup	1
-#define _SerLSavail	2
-#define _SerLSnone	3
-#define _SerCompass	4
-#define _SerAlti	5
-
-#define _SerHelp	6
-#define _SerRxTest	7
-#define _SerRxNN	8
-#define _SerRxRes	9
-#define _SerRxFail	10
-#define _SerRxOK	11
-
-#define _SerAnTest	12
-#define _SerLinTst	13
-#define _SerLinErr	14
-
-#define _SerSrvRun	15
-
-#define _SerI2CRun	16
-#define _SerI2CCnt	17
-
-#define _SerMagTst	18
-
-#define _SerPowTst	19
-#define _SerPowAux1	20
-#define _SerPowAux2	21
-#define _SerPowAux3	22
-#define _SerPowBlue	23
-#define _SerPowRed	24
-#define _SerPowGreen	25
-#define _SerPowYellow	26
-#define _SerPowBeep		27
-
-#define _SerSrvOK	28
-#define _SerPrompt	29
-
-#define _SerVolt	30
-#define _SerFail	31
-#define _SerGrad	32
-#define _SerI2CFail	33
-#define _SerMS		34
-
-#define _SerPPMP	35
-#define _SerPPMN	36
-
-#define _SerLinG	37
-
-#define	_SerBaroOK	38
-#define	_SerBaroT	39
-
-#define _SerCCalib1 40
-#define _SerCCalib2 41
-#define _SerCCalib3 42
-#define _SerCCalibE 43
-
-#define _SerBaroSMD500 44
-#define _SerBaroBMP085 45
-#define _SerBaroComp 46
-
 // defines for SendComValUL
-#define NKSMASK 0b0.0.000.111
-#define	NKS0	0b0.0.000.000
-#define	NKS1	0b0.0.000.001
-#define	NKS2	0b0.0.000.010
-#define	NKS3	0b0.0.000.011
-#define	NKS4	0b0.0.000.100
+#define NKSMASK 0b00000111
+#define	NKS0	0b00000000
+#define	NKS1	0b00000001
+#define	NKS2	0b00000010
+#define	NKS3	0b00000011
+#define	NKS4	0b00000100
 
-#define LENMASK 0b0.0.111.000
-#define LEN5	0b0.0.101.000
-#define LEN4	0b0.0.100.000
-#define LEN3	0b0.0.011.000
-#define LEN2	0b0.0.010.000
+#define LENMASK 0b00111000
+#define LEN5	0b00101000
+#define LEN4	0b00100000
+#define LEN3	0b00011000
+#define LEN2	0b00010000
 
-#define VZ		0b0.1.000.000
+#define VZ		0b01000000
+
+extern const char  SerHello[];
+extern const char  SerSetup[];
+extern const char  SerLSavail[];
+extern const char SerLSnone[];
+extern const char SerCompass[];
+extern const char SerAlti[];
+extern const char SerHelp[];
+extern const char SerAnTest[];
+extern const char SerLinTst[];
+extern const char SerLinErr[];
+extern const char SerSrvRun[];
+extern const char SerI2CRun[];
+extern const char SerI2CCnt[];
+extern const char SerMagTst[];
+extern const char SerPowTst[];
+extern const char SerPowAux1[];
+extern const char SerPowAux2[];
+extern const char SerPowAux3[];
+extern const char SerPowBlue[];
+extern const char SerPowRed[];
+extern const char SerPowGreen[];
+extern const char SerPowYellow[];
+extern const char SerPowBeep[];
+extern const char SerSrvOK[];
+extern const char SerPrompt[];
+extern const char SerVolt[];
+extern const char SerFail[];
+extern const char SerGrad[];
+extern const char SerI2CFail[];
+extern const char SerMS[];
+extern const char SerPPMP[];
+extern const char SerPPMN[];
+extern const char SerLinG[];
+extern const char SerBaroOK[];
+extern const char SerBaroT[];
+extern const char SerCCalib1[];
+extern const char SerCCalib2[]; 
+extern const char SerCCalib3[];
+extern const char SerCCalibE[];
+extern const char SerBaroSMD500[];
+extern const char SerBaroBMP085[];
+extern const char SerBaroComp[];
+
+extern const char SerRxTest[];
+extern const char SerRxRes[];
+extern const char SerRxFail[];
+extern const char SerRxOK[];
+extern const char SerRxNN[];
 
 // Prototypes
 
-// Bank 0
+extern	 void BootStart(void);
 
-extern	page0	void BootStart(void);
+extern	 void ShowSetup(uns8);
+extern	 char RecvComChar(void);
+extern	 void ProcessComCommand(void);
+extern	 void SendComChar(char);
+extern	 void SendComCRLF(void);
+extern	 void SendComValH(uns8);
+extern	 void SendComValUL(uns8);
+extern	 void AnalogTest(void);
+extern	 void OutSignals(void);
+extern	 void TestServos(void);
+extern	 void ConfigureESCs(void);
 
-extern	page0	void ShowSetup(uns8);
-extern	page0	char RecvComChar(void);
-extern	page0	void ProcessComCommand(void);
-extern	page0	void SendComChar(char);
-extern	page0	void SendComCRLF(void);
-extern	page0	void SendComValH(uns8);
-extern	page0	void SendComValUL(uns8);
-extern	page1	void AnalogTest(void);
-extern	page0	void OutSignals(void);
-extern	page0	void TestServos(void);
-extern	page0	void ConfigureESCs(void);
+extern	 void ReceiverTest(void);
+extern	 void TogglePPMPolarity(void);
 
-// Bank 1
-extern	page1	void ReceiverTest(void);
-extern	page1	void TogglePPMPolarity(void);
+extern	 void Delay100mS(uns8);
+extern	 void AcqTime(void);
+extern	 void SendLeds(void);
+extern	 void SwitchLedsOn(uns8);
+extern	 void SwitchLedsOff(uns8);
+extern	 uns8 ScanI2CBus(void);
+extern	 void CompassTest(void);
+extern	 void CalibrateCompass(void);
+extern	 void BaroTest(void);
+extern	 void I2CStart(void);
+extern	 void I2CStop(void);
+extern	 uns8 SendI2CByte(uns8);
+extern	 uns8 RecvI2CByte(uns8);
+extern	 void PowerOutput(uns8);
+extern	 void SendComText(const char *);
 
-extern	page1	void Delay100mS(uns8);
-extern	page1	void AcqTime(void);
-extern	page1	void SendLeds(void);
-extern	page1	void SwitchLedsOn(uns8);
-extern	page1	void SwitchLedsOff(uns8);
-extern	page1	uns8 ScanI2CBus(void);
-extern	page1	void CompassTest(void);
-extern	page1	void CalibrateCompass(void);
-extern	page1	void BaroTest(void);
-extern	page1	void I2CStart(void);
-extern	page1	void I2CStop(void);
-extern	page1	uns8 SendI2CByte(uns8);
-extern	page1	uns8 RecvI2CByte(uns8);
-extern	page1	void PowerOutput(uns8);
-extern	page2	void SendComText(uns8);
+extern	 void ReadParametersEE(void);
 
-extern	page2	void ReadEEdata(void);
+extern	int16 ADC(uns8, uns8);
+extern	void InitADC(void);
 
-// Bank 2
-extern	page2	void IsLISLactive(void);
-extern	page2	void LinearTest(void);
+extern	 void IsLISLactive(void);
+extern	 void LinearTest(void);
+
+extern	 void nop2(void);
 
 // End of pu-test.h
 
