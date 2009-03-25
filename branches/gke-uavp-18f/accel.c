86e799bdadf73b32aa375845a430889524134bf2
gke3500 <gke3500@6150ef54-a058-11dd-a159-232c7ab52622>
2009-03-25 07:49:58 +0000

    Reinstated Gary's mixing option.  Fault in Saturation check with int8 instead of uns8 in 18f version. cc5x probably does not care.

--- branches/gke-uavp-18f/accel.c
+++ branches/gke-uavp-18f/accel.c
@@ -1,8 +1,8 @@
 // =======================================================================
 // =                   U.A.V.P Brushless UFO Controller                  =
 // =                         Professional Version                        =
-// =               Copyright (c) 2008-9 by Prof. Greg Egan               =
-// =     Original V3.15 Copyright (c) 2007 Ing. Wolfgang Mahringer       =
+// =             Copyright (c) 2007 Ing. Wolfgang Mahringer              =
+// =           Extensively modified 2008-9 by Prof. Greg Egan            =
 // =                          http://www.uavp.org                        =
 // =======================================================================
 //
@@ -22,49 +22,31 @@
 
 // Accelerator sensor routine
 
+#pragma codepage=2
 #include "c-ufo.h"
 #include "bits.h"
 
-void GetNeutralAccelerations(void)
+// Math Library
+#include "mymath16.h"
+
+// read all acceleration values from LISL sensor
+// and compute correction adders (Rp, Pp, Vud)
+void CheckLISL(void)
 {
-	// this routine is called ONLY ONCE while booting
-	// read 16 time all 3 axis of linear sensor.
-	// Puts values in Neutralxxx registers.
-	uint8 i;
+	int16 nila1@nilarg1;
 
-	Delay100mSWithOutput(2);	// wait 1/10 sec until LISL is ready to talk
-	// already done in caller program
-	Rp = 0;
-	Pp = 0;
-	Yp = 0;
-	for( i=0; i < 16; i++)
-	{
-		while( (ReadLISL(LISL_STATUS + LISL_READ) & 0x08) == 0 );
-		ReadAccelerations();
-		
-		Rp += Ax;
-		Pp += Az;
-		Yp += Ay;
-	}
-	Rp = SRS16(Rp + 8, 4);
-	Pp = SRS16(Pp + 8, 4);
-	Yp = SRS16(Yp + 8, 4);
-
-	NeutralLR = Limit(Rp, -99, 99);
-	NeutralFB = Limit(Pp, -99, 99);
-	NeutralUD = Limit(Yp-1024, -99, 99); // -1g
-} // GetNeutralAccelerations
-
-void AccelerationCompensation(void)
-{
 	if( _UseLISL )
 	{
-		ReadAccelerations();
-
-		Rp = Ax;
-		Yp = Ay;
-		Pp = Az;;
-		
+		ReadLISL(LISL_STATUS + LISL_READ);
+		// the LISL registers are in order here!!
+		Rp.low8  = (int8)ReadLISL(LISL_OUTX_L + LISL_INCR_ADDR + LISL_READ);
+		Rp.high8 = (int8)ReadLISLNext();
+		Yp.low8  = (int8)ReadLISLNext();
+		Yp.high8 = (int8)ReadLISLNext();
+		Pp.low8  = (int8)ReadLISLNext();
+		Pp.high8 = (int8)ReadLISLNext();
+		LISL_CS = 1;	// end transmission
+			
 		// NeutralLR ,NeutralFB, NeutralUD pass through UAVPSet 
 		// and come back as MiddleLR etc.
 	
@@ -73,56 +55,102 @@ void AccelerationCompensation(void)
 		Pp -= MiddleFB;
 		Yp -= MiddleUD;
 	
+		#if 0
+		// calc the angles for roll matrices
+		// rw = arctan( x*16/z/4 )
+		niltemp = Yp * 16;
+		niltemp1 = niltemp / Rp;
+		niltemp1 >>= 2;
+		nila1 = niltemp1;
+		Rw = Arctan();
+		
+		SendComValS(Rw);
+		SendComChar(';');
+		
+		niltemp1 = niltemp / Pp;
+		niltemp1 >>= 2;
+		nila1 = niltemp1;
+		Pw = Arctan();
+		
+		SendComValS(Pw);
+		SendComChar(0x13);
+		SendComChar(0x10);
+	
+		#endif
+		
 		Yp -= 1024;	// subtract 1g
-	
-
-		#ifdef ACCEL_VUD
-		// UDSum rises if ufo climbs
-		UDSum += Yp;
-	
-		Yp = SRS16(SRS16(UDSum + 8, 4) * (int16) LinUDIntFactor + 128, 8);
-	
-		if( (BlinkCount & 0x03) == 0 )	
-		{
-			if( Yp > Vud ) 
-				Vud++;
-			else
-				if( Yp < Vud )
-					Vud--;
-	
-			Vud = Limit(Vud, -10, 10); // was 20
-		}
-	
-		UDSum = DecayBand(UDSum, -10, 10, 10);
-	
-		#endif // ACCEL_VUD
 
 		#ifdef DEBUG_SENSORS
 		if( IntegralCount == 0 )
 		{
-			Trace[TAx]= Rp;
-			Trace[TAz] = Pp;
-			Trace[TAy] = Yp;
-
-			Trace[TUDSum] = UDSum;
-			Trace[TVud] = Vud;
+			SendComValH(Rp.high8);
+			SendComValH(Rp.low8);
+			SendComChar(';');
+			SendComValH(Pp.high8);
+			SendComValH(Pp.low8);
+			SendComChar(';');
+			SendComValH(Yp.high8);
+			SendComValH(Yp.low8);
+			SendComChar(';');
 		}
 		#endif
 	
-		// Roll
-
+		// DO NOT USE - Requires roll & pitch angle compensation
+		#ifdef NADA
+		// UDSum rises if ufo climbs
+		UDSum += Yp - RollPitchComp;
+	
+		Yp = UDSum;
+		Yp += 8;
+		Yp >>= 4;
+		Yp *= LinUDIntFactor;
+		Yp += 128;
+	
+		if( (BlinkCount & 0x03) == 0 )	
+		{
+			if( (int8)Yp.high8 > Vud ) 
+				Vud++;
+			else
+				if( (int8)Yp.high8 < Vud )
+					Vud--;
+	
+			if( Vud >  10 ) // was 20
+				Vud =  10; 
+			else
+				if( Vud < -10 ) 
+					Vud = -10;
+		}
+		if( UDSum >  10 ) 
+			UDSum -= 10;
+		else
+			if( UDSum < -10 ) 
+				UDSum += 10;
+		#endif // ACCEL_VUD
+	
+		// =====================================
+		// Roll-Axis
+		// =====================================
 		// Static compensation due to Gravity
+	
 		#ifdef OPT_ADXRS
-		Rp -= SRS16(RollSum * 11 + 16, 5);
-		#else // OPT_IDG
-		Rp -= SRS16(RollSum * (-15) + 16, 5); 
+		Yl = RollSum * 11;	// Rp um RollSum*11/32 korrigieren
 		#endif
+	
+		#ifdef OPT_IDG
+		Yl = RollSum * -15; // Rp um RollSum* -15/32 korrigieren
+		#endif
+		Yl += 16;
+		Yl >>= 5;
+		Rp -= Yl;
 	
 		// dynamic correction of moved mass
 		#ifdef OPT_ADXRS
-		Rp += (int16)RollRate << 1;
-		#else // OPT_IDG
-		Rp -= (int16)RollRate;
+		Rp += (int16)RollSamples;
+		Rp += (int16)RollSamples;
+		#endif
+	
+		#ifdef OPT_IDG
+		Rp -= (int16)RollSamples;
 		#endif
 	
 		// correct DC level of the integral
@@ -131,33 +159,53 @@ void AccelerationCompensation(void)
 		if( Rp > 10 ) LRIntKorr =  1;
 		else
 			if( Rp < 10 ) LRIntKorr = -1;
-		#else // OPT_IDG
+		#endif
+		#ifdef OPT_IDG
 		if( Rp > 10 ) LRIntKorr = -1;
 		else
 			if( Rp < 10 ) LRIntKorr =  1;
 		#endif
 	
-		// Pitch
-
-		Pp = -Pp;				// long standing BUG ???
-
+		#ifdef NADA
+		// Integral addieren, Abkling-Funktion
+		Yl = LRSum >> 4;
+		Yl >>= 1;
+		LRSum -= Yl;	// LRSum * 0.96875
+		LRSum += Rp;
+		Rp = LRSum + 128;
+		LRSumPosi += (int8)Rp.high8;
+		if( LRSumPosi >  2 ) LRSumPosi -= 2;
+		else
+			if( LRSumPosi < -2 ) LRSumPosi += 2;
+	
+		// Korrekturanteil fuer den PID Regler
+		Rp = LRSumPosi * LinLRIntFactor;
+		Rp += 128;
+		Rp = (int8)Rp.high8;
+		// limit output
+		if( Rp >  2 ) Rp = 2;
+		else
+			if( Rp < -2 ) Rp = -2;
+		#endif // NADA
+	
+		// =====================================
+		// Pitch-Axis
+		// =====================================
 		// Static compensation due to Gravity
+
+		Pp =-Pp; // long standing error???
+	
 		#ifdef OPT_ADXRS
-		Pp -= SRS16(PitchSum * 11 + 16, 5);	
-		#else // OPT_IDG
-		Pp -= SRS16(PitchSum * (-15) + 16, 5);
+		Yl = PitchSum * 11;	// Pp um RollSum* 11/32 korrigieren
 		#endif
-		
-	#ifdef PITCH_MM_COMP
-		// dynamic correction of moved mass
-		#ifdef OPT_ADXRS
-		Pp += (int16)PitchRate << 1;
-		#else // OPT_IDG
-		Pp -= (int16)PitchRate;
+		#ifdef OPT_IDG
+		Yl = PitchSum * -15;	// Pp um RollSum* -14/32 korrigieren
 		#endif
-	#else
-		// no dynamic correction of moved mass
-	#endif // PITCH_MM_COMP
+		Yl += 16;
+		Yl >>= 5;
+	
+		Pp -= Yl;
+		// no dynamic correction of moved mass necessary
 	
 		// correct DC level of the integral
 		FBIntKorr = 0;
@@ -170,16 +218,42 @@ void AccelerationCompensation(void)
 		if( Pp > 10 ) FBIntKorr = -1;
 		else
 			if( Pp < 10 ) FBIntKorr =  1;
-		#endif		
-	}	
+		#endif
+	
+		#ifdef NADA
+		// Integral addieren
+		// Integral addieren, Abkling-Funktion
+		Yl = FBSum >> 4;
+		Yl >>= 1;
+		FBSum -= Yl;	// LRSum * 0.96875
+		FBSum += Pp;
+		Pp = FBSum + 128;
+		FBSumPosi += (int8)Pp.high8;
+		if( FBSumPosi >  2 ) FBSumPosi -= 2;
+		else
+			if( FBSumPosi < -2 ) FBSumPosi += 2;
+	
+		// Korrekturanteil fuer den PID Regler
+		Pp = FBSumPosi * LinFBIntFactor;
+		Pp += 128;
+		Pp = (int8)Pp.high8;
+		// limit output
+		if( Pp >  2 ) 
+			Pp = 2;
+		else
+			if( Pp < -2 ) 
+				Pp = -2;
+		#endif // NADA
+	}
 	else
 	{
 		Vud = 0;
+
 		#ifdef DEBUG_SENSORS
-		Trace[TAx] = 0;
-		Trace[TAz] = 0;
-		Trace[TAy] = 0;	
+		SendComChar(';');
+		SendComChar(';');
+		SendComChar(';');
 		#endif
 	}
-} // AccelerationCompensation
 
+} // CheckLISL
--- branches/gke-uavp-18f/c-ufo.c
+++ branches/gke-uavp-18f/c-ufo.c
@@ -1,8 +1,8 @@
 // =======================================================================
 // =                   U.A.V.P Brushless UFO Controller                  =
 // =                         Professional Version                        =
-// =               Copyright (c) 2008-9 by Prof. Greg Egan               =
-// =     Original V3.15 Copyright (c) 2007 Ing. Wolfgang Mahringer       =
+// =             Copyright (c) 2007 Ing. Wolfgang Mahringer              =
+// =           Extensively modified 2008-9 by Prof. Greg Egan            =
 // =                          http://www.uavp.org                        =
 // =======================================================================
 //
@@ -25,101 +25,149 @@
 // -CC -fINHX8M -a -L -Q -V -FM -DMATHBANK_VARS=bank0 -DMATHBANK_PROG=2
 
 #ifdef ICD2_DEBUG
-#pragma	config OSC=HS, WDT=OFF, MCLRE=OFF, LVP=OFF, PBADEN=OFF, CCP2MX = PORTC
+#pragma	config = 0x377A	// BODEN, HVP, no WDT, MCLRE disabled, PWRTE disabled
 #else
-#pragma	config OSC=HS, WDT=OFF, PWRT=ON, MCLRE=OFF, LVP=OFF, PBADEN=OFF, CCP2MX = PORTC 
+#pragma	config = 0x3F72	// BODEN, HVP, no WDT, MCLRE disabled, PWRTE enabled
 #endif
 
 #include "c-ufo.h"
 #include "bits.h"
 
+#pragma codepage = 0
+
+// Math Library
+#include "mymath16.h"
+
+// EEPROM-Data can currently not be used with relocatable assembly
+// CC5X limitation!
+// #define EEPROM_START	0x2100
+// #pragma cdata[EEPROM_START] = 0,0,0,0,0,0,0,0,0,0,20,8,16
+
 // The globals
 
-uint8	IGas;			// actual input channel, can only be positive!
+uns8	IGas;			// actual input channel, can only be positive!
 int8 	IRoll,IPitch,IYaw;	// actual input channels, 0 = neutral
-uint8	IK5;						// actual channel 5 input
-uint8	IK6;						// actual channel 6 input
-uint8	IK7;						// actual channel 7 input
+uns8	IK5;		// actual channel 5 input
+uns8	IK6;		// actual channel 6 input
+uns8	IK7;		// actual channel 7 input
 
 // PID Regler Variablen
-int16	RE, PE, YE;					// gyro rate error	
-int16	REp, PEp, YEp;				// previous error for derivative
-int16	RollSum, PitchSum, YawSum;	// integral 	
-int16	RollRate, PitchRate;
-int16	AverageYawRate, YawRate;
-int16	MidRoll, MidPitch, MidYaw;
-int16	Ax, Ay, Az;
-int8	LRIntKorr, FBIntKorr;
-int8	NeutralLR, NeutralFB, NeutralUD;
-int16 	UDSum;
+int8		RE,PE;
+int8		YE;	// Fehlersignal aus dem aktuellen Durchlauf
+int8		REp,PEp;
+int8		YEp;	// Fehlersignal aus dem vorigen Durchlauf (war RE/PE/YE)
+int16	YawSum;	// Integrales Fehlersignal fuer Yaw, 0 = neutral
+int16	RollSum, PitchSum;	// Integrales Fehlersignal fuer Roll und Pitch
+uns16	RollSamples, PitchSamples;
+int8		LRIntKorr, FBIntKorr;
+uns8	NeutralLR, NeutralFB, NeutralUD;
+int8 	UDSum;
 
-// Failsafes
-int8	RollNeutral, PitchNeutral, YawNeutral;
-uint8	ThrNeutral;
+int8		NegFact; // general purpose
+uns8	BlinkCount, BlinkCycle, BaroCount;
+int16	niltemp1;
+int16	niltemp;
+int8		BatteryVolts; // added by Greg Egan
+int8		Rw,Pw;
 
-// Variables for barometric sensor PD-controller
-int24	BaroBasePressure, BaroBaseTemp;
-int16   BaroRelPressure, BaroRelTempCorr;
-uint16	BaroVal;
-int16	VBaroComp;
-uint8	BaroType, BaroTemp, BaroRestarts;
+uns16	BaroBasePressure, BaroBaseTemp, BaroRelTempCorr;
+int8		VBaroComp;
+int8 	BaroRelPressure;
+uns8	BaroType, BaroTemp, BaroRestarts;
 
-uint8	LedShadow;		// shadow register
-int16	AbsDirection;	// wanted heading (240 = 360 deg)
-int16	CurDeviation;	// deviation from correct heading
 
-uint8	MFront,MLeft,MRight,MBack;	// output channels
-uint8	MCamRoll,MCamPitch;
+// Die Reihenfolge dieser Variablen MUSS gewahrt bleiben!!!!
+int8	RollPropFactor;
+int8	RollIntFactor;
+int8	RollDiffFactor;
+int8 RollLimit;
+int8 RollIntLimit;
+
+int8	PitchPropFactor;
+int8	PitchIntFactor;
+int8	PitchDiffFactor;
+int8 PitchLimit;
+int8 PitchIntLimit;
+
+int8	YawPropFactor;
+int8	YawIntFactor;
+int8	YawDiffFactor;
+int8	YawLimit;
+int8 YawIntLimit;
+
+int8	ConfigParam;
+int8	TimeSlot;
+int8 LowVoltThres;
+
+int8	LinLRIntFactor;
+int8	LinFBIntFactor;
+int8	LinUDIntFactor;
+int8 MiddleUD;
+int8 MotorLowRun;
+int8	MiddleLR;
+int8 MiddleFB;
+int8	CamPitchFactor;
+int8 CompassFactor;
+
+int8 BaroThrottleDiff;
+
+// Ende Reihenfolgezwang
+
+uns16	MidRoll, MidPitch, MidYaw;
+
+
+uns8	LedShadow;	// shadow register
+uns16	AbsDirection;	// wanted heading (240 = 360 deg)
+int8		CurDeviation;	// deviation from correct heading
+
+uns8	MFront,MLeft,MRight,MBack;	// output channels
+uns8	MCamRoll,MCamPitch;
 int16	Ml, Mr, Mf, Mb;
-int16	Rl,Pl,Yl;		// PID output values
+int16	Rl,Pl,Yl;	// PID output values
 int16	Rp,Pp,Yp;
 int16	Vud;
 
-int16	Trace[LastTrace];
+uns8	Flags;
+uns8	Flags2;
 
-uint8	Flags[8];
-uint8	Flags2[8];
+uns8	IntegralCount;
+int8		RollNeutral, PitchNeutral, YawNeutral;
+uns8	ThrNeutral;
+uns16	ThrDownCount;
 
-int16	IntegralCount, ThrDownCount, DropoutCount, LedCount, BlinkCount, BlinkCycle, BaroCount;
-uint24	RCGlitchCount;
-int8	BatteryVolts;
-int8	Rw,Pw;
+uns8	DropoutCount;
+uns8	LedCount;
 
-#pragma idata params
-// Principal quadrocopter parameters - MUST remain in this order
-// for block read/write to EEPROM
-int8	RollPropFactor		=18;
-int8	RollIntFactor		=4;
-int8	RollDiffFactor		=-40;
-int8	BaroTempCoeff		=13;
-int8	RollIntLimit		=16;
-int8	PitchPropFactor		=18;
-int8	PitchIntFactor		=4;	
-int8	PitchDiffFactor		=-40;	 
-int8	BaroThrottleProp	=2;
-int8	PitchIntLimit		=16;
-int8	YawPropFactor		=20;
-int8	YawIntFactor		=40;
-int8	YawDiffFactor		=6;
-int8	YawLimit			=50;
-int8	YawIntLimit			=6;
-int8	ConfigParam			=0b00000000;
-int8	TimeSlot			=4;	// control update interval + LEGACY_OFFSET
-int8	LowVoltThres		=43;
-int8	CamRollFactor		=4;	
-int8	LinFBIntFactor		=0;	// unused
-int8	LinUDIntFactor		=8; // unused
-int8	MiddleUD			=0;
-int8	MotorLowRun			= 40;
-int8	MiddleLR			=0;
-int8	MiddleFB			=0;
-int8	CamPitchFactor		=4;
-int8	CompassFactor		=5;
-int8	BaroThrottleDiff	=4;
-#pragma idata
-
-
-// Ende Reihenfolgezwang
+void LedGame(void)
+{
+	if( --LedCount == 0 )
+	{
+		LedCount = 255-IGas;	// new setup
+		LedCount >>= 3;
+		LedCount += 5;
+		if( _Hovering )
+		{
+			AUX_LEDS_ON;	// baro locked, all aux-leds on
+		}
+		else
+		if( LedShadow & LedAUX1 )
+		{
+			AUX_LEDS_OFF;
+			LedAUX2_ON;
+		}
+		else
+		if( LedShadow & LedAUX2 )
+		{
+			AUX_LEDS_OFF;
+			LedAUX3_ON;
+		}
+		else
+		{
+			AUX_LEDS_OFF;
+			LedAUX1_ON;
+		}
+	}
+} // LedGame
 
 void WaitThrottleClosed(void)
 {
@@ -131,7 +179,7 @@ void WaitThrottleClosed(void)
 		if( _NewValues )
 		{
 			OutSignals();
-			_NewValues = false;
+			_NewValues = 0;
 			if( --DropoutCount <= 0 )
 			{
 				LedRed_TOG;	// toggle red Led 
@@ -145,13 +193,11 @@ void WaitThrottleClosed(void)
 
 void CheckThrottleMoved(void)
 {
-	int16 Temp;
-
 	if( _NewValues )
 	{
 		if( ThrDownCount > 0 )
 		{
-			if( (LedCount & 1) == 0 )
+			if( LedCount.0 == 0 )
 				ThrDownCount--;
 			if( ThrDownCount == 0 )
 				ThrNeutral = IGas;	// remember current Throttle level
@@ -159,14 +205,14 @@ void CheckThrottleMoved(void)
 		else
 		{
 			if( ThrNeutral < THR_MIDDLE )
-				Temp = 0;
+				NegFact = 0;
 			else
-				Temp = ThrNeutral - THR_MIDDLE;
+				NegFact = ThrNeutral - THR_MIDDLE;
 
 			if( IGas < THR_HOVER ) // no hovering below this throttle setting
 				ThrDownCount = THR_DOWNCOUNT;	// left dead area
 
-			if( IGas < Temp )
+			if( IGas < NegFact )
 				ThrDownCount = THR_DOWNCOUNT;	// left dead area
 			if( IGas > ThrNeutral + THR_MIDDLE )
 				ThrDownCount = THR_DOWNCOUNT;	// left dead area
@@ -182,86 +228,94 @@ void WaitForRxSignal(void)
 		Delay100mSWithOutput(2);	// wait 2/10 sec until signal is there
 		ProcessComCommand();
 		if( _NoSignal )
+		{
 			if( Switch )
 			{
 				if( --DropoutCount == 0 )
 				{
-					_LostModel = true;
+					_LostModel = 1;
 					DropoutCount = MODELLOSTTIMERINT;
 				}
 			}
 			else
-				_LostModel = false;
+				_LostModel = 0;
+		}
 	}
 	while( _NoSignal || !Switch);	// no signal or switch is off
 } // WaitForRXSignal
 
 void main(void)
 {
-	uint8	i;
-	uint8	LowGasCount;
+	uns8	LowGasCount;
 
-	DisableInterrupts;
+	OPTION_REG = 0b.00000.011;	// TMR0 w/ int clock, 1:16 presc (see _PreScale0),
+					// weak pullups on
+	// general ports setup
+	TRISA = 0b.00111111;	// all inputs
+	ADCON1 = 0b.0.000.0010;	// uses 5V as Vref
 
-	InitPorts();
-	OpenUSART(USART_TX_INT_OFF&USART_RX_INT_OFF&USART_ASYNCH_MODE&
-			USART_EIGHT_BIT&USART_CONT_RX&USART_BRGH_HIGH, _B38400);
-	
-	InitADC();
-	
-	OpenTimer0(TIMER_INT_OFF&T0_8BIT&T0_SOURCE_INT&T0_PS_1_16);
-	OpenTimer1(T1_8BIT_RW&TIMER_INT_OFF&T1_PS_1_8&T1_SYNC_EXT_ON&T1_SOURCE_CCP&T1_SOURCE_INT);
-
-	OpenCapture1(CAPTURE_INT_ON & C1_EVERY_FALL_EDGE); 	// capture mode every falling edge
-	CCP1CONbits.CCP1M0 = NegativePPM;
-
-	OpenTimer2(TIMER_INT_ON&T2_PS_1_16&T2_POST_1_16);		
-	PR2 = TMR2_5MS;		// set compare reg to 9ms
-
-	// setup flags register
-	for ( i = 8; i ; i-- )
-		Flags2[i] = Flags[i] = false; 
-	_NoSignal = true;		// assume no signal present
-
+	PORTB = 0b.1100.0000;		// all outputs to low, except RB6 & 7 (I2C)!
+	TRISB = 0b.0100.0000;	// all servo and LED outputs
+	PORTC = 0b.0110.0000;		// all outputs to low, except TxD and CS
+	TRISC = 0b.10000100;	// RC7, RC2 are inputs
+	RBPU_ = 1;			// disable weak pullups
+	CKE = 1;		// default I2C - enable SMBus thresholds for 3.3V LISL
 	LedShadow = 0;
     ALL_LEDS_OFF;
 
+	// setup serial port for 8N1
+	TXSTA = 0b.0010.0100;	// async mode, BRGH = 1
+	RCSTA = 0b.1001.0000;	// receive mode
+	SPBRG = _B38400;
+	//SPBRG = _B115200;
+	//SPBRG = _B230400;
+
+	W = RCREG;			// be sure to empty FIFO
+
+	// timer setup
+	T1CON = 0b.00.11.0001;	// enable TMR1, 1:8 prescaler, run with 2us clock
+	CCP1CON = 0b.00.00.0100;	// capture mode every falling edge
+
+	TMR2 = 0;
+	T2CON = 0b.0.1111.1.11;	// enable TMR2, 1:16 prescaler, 1:16 postscaler (see _Pre/PostScale2)
+	PR2 = TMR2_5MS;		// set compare reg to 9ms
+
+	// setup flags register
+	Flags = 0;
+	Flags2 = 0;
+	_NoSignal = 1;		// assume no signal present
+
 	InitArrays();
 
-	#ifdef INIT_PARAMS
-	for (i=_EESet2*2; i ; i--)					// clear EEPROM parameter space
-		WriteEE(i, -1);
-	WriteParametersEE(1);						// copy RAM initial values to EE
-	WriteParametersEE(2);
-	#endif // INIT_PARAMS
-	ReadParametersEE();
-
-	INTCONbits.PEIE = true;		// Enable peripheral interrupts
-	EnableInterrupts;
-
-	LedRed_ON;
+	LedRed_ON;		// red LED on
 	Delay100mSWithOutput(1);	// wait 1/10 sec until LISL is ready to talk
 
 	#ifdef USE_ACCSENS
 	IsLISLactive();
 	#ifdef ICD2_DEBUG
-	_UseLISL = true;	// because debugger uses RB7 (=LISL-CS) :-(
+	_UseLISL = 1;	// because debugger uses RB7 (=LISL-CS) :-(
 	#endif
 
 	NeutralLR = 0;
 	NeutralFB = 0;
 	NeutralUD = 0;
 	if( _UseLISL )
-		GetNeutralAccelerations();	// into Rp, Pp, Yp
+		GetEvenValues();	// into Rp, Pp, Yp
 	#endif  // USE_ACCSENS
+
+	// enable the interrupts
+	CCP1IE = 1;
+	TMR2IE = 1;		// T1-Capture and T2 interrupt enable
+		
+	PEIE = 1;		// Enable peripheral interrupts
 
 	ThrNeutral = 0xFF;
 
 	// send hello text to serial COM
 	Delay100mSWithOutput(1);	// just to see the output after powerup
 
-	InitDirection();
-	InitBarometer();
+	InitDirection();	// init compass sensor
+	InitAltimeter();
 
 	ShowSetup(1);
 
@@ -276,21 +330,21 @@ Restart:
 
 	while(1)
 	{
-		INTCONbits.TMR0IE = false;		// Disable TMR0 interrupt
+		T0IE = 0;		// Disable TMR0 interrupt
 
 		ALL_LEDS_OFF;
 		LedRed_ON;		// Red LED on
-		if( _UseLISL )
+		if(_UseLISL)
 			LedYellow_ON;	// To signal LISL sensor is active
 
 		InitArrays();
 		ThrNeutral = 0xFF;
 
-		EnableInterrupts;		// Enable all interrupts
+		GIE = 1;		// Enable all interrupts
 	
 		WaitForRxSignal(); // Wait until a valid RX signal is received
 
-		ReadParametersEE();
+		ReadEEdata();
 
 		WaitThrottleClosed();
 
@@ -313,35 +367,34 @@ Restart:
 		// else assume use for camera roll trim	
 		_UseCh7Trigger = IK7 < 30;
 			
-		while ( Switch == 1 )
+		while ( Switch == 1 )	// as int16 as power switch is ON
 		{
 			// wait pulse pause delay time (TMR0 has 1024us for one loop)
-			WriteTimer0(0);
-			INTCONbits.TMR0IF = false;
-			INTCONbits.TMR0IE = true;	// enable TMR0
+			TMR0 = 0;
+			T0IF = 0;
+			T0IE = 1;	// enable TMR0
 
 			UpdateBlinkCount();
-			RollRate =PitchRate = 0;	// zero gyros sum-up memory
+			RollSamples =PitchSamples = 0;	// zero gyros sum-up memory
 			// sample gyro data and add everything up while waiting for timing delay
 
 			GetGyroValues();
+			GetDirection();	
+			ComputeBaroComp();
 
 			while( TimeSlot > 0 )
 			{
 				// Here is the place to insert own routines
-				// It should consume as little time as possible!
+				// It should consume as less time as possible!
 				// ATTENTION:
 				// Your routine must return BEFORE TimeSlot reaches 0
 				// or non-optimal flight behavior might occur!!!
 			}
 
-			GetDirection();	
-			ComputeBaroComp();
-
-			INTCONbits.TMR0IE = false;	// disable timer
+			T0IE = 0;	// disable timer
 			GetGyroValues();
 
-			ReadParametersEE();	// re-sets TimeSlot
+			ReadEEdata();	// re-sets TimeSlot
 
 			CalcGyroValues();
 
@@ -353,7 +406,7 @@ Restart:
 				if( DropoutCount < MAXDROPOUT )
 				{	// FAILSAFE	
 					// hold last throttle
-					_LostModel = true;
+					_LostModel = 1;
 					ALL_LEDS_OFF;
 					IRoll = RollNeutral;
 					IPitch = PitchNeutral;
@@ -412,12 +465,14 @@ Restart:
 					LedCount = 1;
 				}
 
-				_Flying = true;
-				_LostModel = false;
+				_Flying = 1;
+				_LostModel = 0;
 				DropoutCount = 0;
 				LowGasCount = 100;		
 				LedGreen_ON;
+
 				LedGame();
+
 DoPID:
 				// do the calculations
 				Rp = 0;
@@ -442,11 +497,15 @@ DoPID:
 			MixAndLimitCam();
 			OutSignals();
 
+			CheckBattery();
 			CheckAlarms();
 
 			#ifdef DEBUG_SENSORS
 			if( IntegralCount == 0 )
-				DumpTrace();
+			{
+				SendComChar(0x0d);
+				SendComChar(0x0a);
+			}
 			#endif			
 
 		}	// END NORMAL OPERATION WHILE LOOP
