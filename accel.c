e1ba0b010af0b68c890eeccc405c0d5a6404e298
gke3500 <gke3500@6150ef54-a058-11dd-a159-232c7ab52622>
2009-03-26 01:49:49 +0000

    Angle compensated vertical velocity damper added with roll/pitch angle limits.

--- accel.c
+++ accel.c
@@ -1,8 +1,8 @@
 // =======================================================================
 // =                   U.A.V.P Brushless UFO Controller                  =
 // =                         Professional Version                        =
-// =             Copyright (c) 2007 Ing. Wolfgang Mahringer              =
-// =           Extensively modified 2008-9 by Prof. Greg Egan            =
+// =               Copyright (c) 2008-9 by Prof. Greg Egan               =
+// =     Original V3.15 Copyright (c) 2007 Ing. Wolfgang Mahringer       =
 // =                          http://www.uavp.org                        =
 // =======================================================================
 //
@@ -22,31 +22,50 @@
 
 // Accelerator sensor routine
 
-#pragma codepage=2
 #include "c-ufo.h"
 #include "bits.h"
 
-// Math Library
-#include "mymath16.h"
-
-// read all acceleration values from LISL sensor
-// and compute correction adders (Rp, Pp, Vud)
-void CheckLISL(void)
+void GetNeutralAccelerations(void)
 {
-	int16 nila1@nilarg1;
+	// this routine is called ONLY ONCE while booting
+	// read 16 time all 3 axis of linear sensor.
+	// Puts values in Neutralxxx registers.
+	uint8 i;
 
+	Delay100mSWithOutput(2);	// wait 1/10 sec until LISL is ready to talk
+	// already done in caller program
+	Rp = 0;
+	Pp = 0;
+	Yp = 0;
+	for( i=0; i < 16; i++)
+	{
+		while( (ReadLISL(LISL_STATUS + LISL_READ) & 0x08) == 0 );
+		ReadAccelerations();
+		
+		Rp += Ax;
+		Pp += Az;
+		Yp += Ay;
+	}
+	Rp = SRS16(Rp + 8, 4);
+	Pp = SRS16(Pp + 8, 4);
+	Yp = SRS16(Yp + 8, 4);
+
+	NeutralLR = Limit(Rp, -99, 99);
+	NeutralFB = Limit(Pp, -99, 99);
+	NeutralUD = Limit(Yp-1024, -99, 99); // -1g
+} // GetNeutralAccelerations
+
+void AccelerationCompensation(void)
+{
+	int16 AbsRollSum, AbsPitchSum, Temp;
 	if( _UseLISL )
 	{
-		ReadLISL(LISL_STATUS + LISL_READ);
-		// the LISL registers are in order here!!
-		Rp.low8  = (int8)ReadLISL(LISL_OUTX_L + LISL_INCR_ADDR + LISL_READ);
-		Rp.high8 = (int8)ReadLISLNext();
-		Yp.low8  = (int8)ReadLISLNext();
-		Yp.high8 = (int8)ReadLISLNext();
-		Pp.low8  = (int8)ReadLISLNext();
-		Pp.high8 = (int8)ReadLISLNext();
-		LISL_CS = 1;	// end transmission
-			
+		ReadAccelerations();
+
+		Rp = Ax;
+		Yp = Ay;
+		Pp = Az;;
+		
 		// NeutralLR ,NeutralFB, NeutralUD pass through UAVPSet 
 		// and come back as MiddleLR etc.
 	
@@ -55,157 +74,89 @@ void CheckLISL(void)
 		Pp -= MiddleFB;
 		Yp -= MiddleUD;
 	
-		#if 0
-		// calc the angles for roll matrices
-		// rw = arctan( x*16/z/4 )
-		niltemp = Yp * 16;
-		niltemp1 = niltemp / Rp;
-		niltemp1 >>= 2;
-		nila1 = niltemp1;
-		Rw = Arctan();
-		
-		SendComValS(Rw);
-		SendComChar(';');
-		
-		niltemp1 = niltemp / Pp;
-		niltemp1 >>= 2;
-		nila1 = niltemp1;
-		Pw = Arctan();
-		
-		SendComValS(Pw);
-		SendComChar(0x13);
-		SendComChar(0x10);
-	
-		#endif
-		
 		Yp -= 1024;	// subtract 1g
+	
+		#ifdef ACCEL_VUD
+		// UDSum rises if ufo climbs
+		// Empirical - vertical acceleration decreases at ~approx Angle/8
+
+		AbsRollSum = Abs(RollSum);
+		AbsPitchSum = Abs(PitchSum);
+
+		if ( (AbsRollSum < 200) && ( AbsPitchSum < 200) ) // ~ 10deg
+			UDSum += Yp + SRS16( AbsRollSum + AbsPitchSum, 3);
+
+		UDSum = Limit(UDSum , -16384, 16384); 
+		UDSum = DecayBand(UDSum, -10, 10, 10);
+	
+		Temp = SRS16(SRS16(UDSum + 8, 4) * (int16) LinUDIntFactor + 128, 8);
+		if( (BlinkCount & 0x03) == 0 )	
+			if( Temp > Vud ) 
+				Vud++;
+			else
+				if( Temp < Vud )
+					Vud--;
+	
+		Vud = Limit(Vud, -10, 10); // was 20
+	
+		#endif // ACCEL_VUD
 
 		#ifdef DEBUG_SENSORS
 		if( IntegralCount == 0 )
 		{
-			SendComValH(Rp.high8);
-			SendComValH(Rp.low8);
-			SendComChar(';');
-			SendComValH(Pp.high8);
-			SendComValH(Pp.low8);
-			SendComChar(';');
-			SendComValH(Yp.high8);
-			SendComValH(Yp.low8);
-			SendComChar(';');
+			Trace[TAx]= Rp;
+			Trace[TAz] = Pp;
+			Trace[TAy] = Yp;
+
+			Trace[TUDSum] = UDSum;
+			Trace[TVud] = Vud;
 		}
 		#endif
 	
-		// DO NOT USE - Requires roll & pitch angle compensation
-		#ifdef NADA
-		// UDSum rises if ufo climbs
-		UDSum += Yp - RollPitchComp;
-	
-		Yp = UDSum;
-		Yp += 8;
-		Yp >>= 4;
-		Yp *= LinUDIntFactor;
-		Yp += 128;
-	
-		if( (BlinkCount & 0x03) == 0 )	
-		{
-			if( (int8)Yp.high8 > Vud ) 
-				Vud++;
-			else
-				if( (int8)Yp.high8 < Vud )
-					Vud--;
-	
-			if( Vud >  10 ) // was 20
-				Vud =  10; 
-			else
-				if( Vud < -10 ) 
-					Vud = -10;
-		}
-		if( UDSum >  10 ) 
-			UDSum -= 10;
-		else
-			if( UDSum < -10 ) 
-				UDSum += 10;
-		#endif // ACCEL_VUD
-	
-		// =====================================
-		// Roll-Axis
-		// =====================================
+		// Roll
+
 		// Static compensation due to Gravity
-	
 		#ifdef OPT_ADXRS
-		Yl = RollSum * 11;	// Rp um RollSum*11/32 korrigieren
+		Rp -= SRS16(RollSum * 11 + 16, 5);
+		#else // OPT_IDG
+		Rp -= SRS16(RollSum * (-15) + 16, 5); 
 		#endif
 	
-		#ifdef OPT_IDG
-		Yl = RollSum * -15; // Rp um RollSum* -15/32 korrigieren
-		#endif
-		Yl += 16;
-		Yl >>= 5;
-		Rp -= Yl;
-	
+		#ifdef DYNAMIC_MASS_COMP
 		// dynamic correction of moved mass
 		#ifdef OPT_ADXRS
-		Rp += (int16)RollSamples;
-		Rp += (int16)RollSamples;
+		Rp += (int16)RollRate << 1;
+		#else // OPT_IDG
+		Rp -= (int16)RollRate;
+		#endif	
+		#else
+		// no dynamic correction of moved mass
 		#endif
-	
-		#ifdef OPT_IDG
-		Rp -= (int16)RollSamples;
-		#endif
-	
+
 		// correct DC level of the integral
 		LRIntKorr = 0;
 		#ifdef OPT_ADXRS
 		if( Rp > 10 ) LRIntKorr =  1;
 		else
 			if( Rp < 10 ) LRIntKorr = -1;
-		#endif
-		#ifdef OPT_IDG
+		#else // OPT_IDG
 		if( Rp > 10 ) LRIntKorr = -1;
 		else
 			if( Rp < 10 ) LRIntKorr =  1;
 		#endif
 	
-		#ifdef NADA
-		// Integral addieren, Abkling-Funktion
-		Yl = LRSum >> 4;
-		Yl >>= 1;
-		LRSum -= Yl;	// LRSum * 0.96875
-		LRSum += Rp;
-		Rp = LRSum + 128;
-		LRSumPosi += (int8)Rp.high8;
-		if( LRSumPosi >  2 ) LRSumPosi -= 2;
-		else
-			if( LRSumPosi < -2 ) LRSumPosi += 2;
-	
-		// Korrekturanteil fuer den PID Regler
-		Rp = LRSumPosi * LinLRIntFactor;
-		Rp += 128;
-		Rp = (int8)Rp.high8;
-		// limit output
-		if( Rp >  2 ) Rp = 2;
-		else
-			if( Rp < -2 ) Rp = -2;
-		#endif // NADA
-	
-		// =====================================
-		// Pitch-Axis
-		// =====================================
-		// Static compensation due to Gravity
+		// Pitch
 
-		Pp =-Pp; // long standing error???
-	
+		Pp = -Pp;				// long standing BUG!
+
+		// Static compensation due to Gravity
 		#ifdef OPT_ADXRS
-		Yl = PitchSum * 11;	// Pp um RollSum* 11/32 korrigieren
+		Pp -= SRS16(PitchSum * 11 + 16, 5);	
+		#else // OPT_IDG
+		Pp -= SRS16(PitchSum * (-15) + 16, 5);
 		#endif
-		#ifdef OPT_IDG
-		Yl = PitchSum * -15;	// Pp um RollSum* -14/32 korrigieren
-		#endif
-		Yl += 16;
-		Yl >>= 5;
-	
-		Pp -= Yl;
-		// no dynamic correction of moved mass necessary
+		
+		// no dynamic correction of moved mass
 	
 		// correct DC level of the integral
 		FBIntKorr = 0;
@@ -218,42 +169,16 @@ void CheckLISL(void)
 		if( Pp > 10 ) FBIntKorr = -1;
 		else
 			if( Pp < 10 ) FBIntKorr =  1;
-		#endif
-	
-		#ifdef NADA
-		// Integral addieren
-		// Integral addieren, Abkling-Funktion
-		Yl = FBSum >> 4;
-		Yl >>= 1;
-		FBSum -= Yl;	// LRSum * 0.96875
-		FBSum += Pp;
-		Pp = FBSum + 128;
-		FBSumPosi += (int8)Pp.high8;
-		if( FBSumPosi >  2 ) FBSumPosi -= 2;
-		else
-			if( FBSumPosi < -2 ) FBSumPosi += 2;
-	
-		// Korrekturanteil fuer den PID Regler
-		Pp = FBSumPosi * LinFBIntFactor;
-		Pp += 128;
-		Pp = (int8)Pp.high8;
-		// limit output
-		if( Pp >  2 ) 
-			Pp = 2;
-		else
-			if( Pp < -2 ) 
-				Pp = -2;
-		#endif // NADA
-	}
+		#endif		
+	}	
 	else
 	{
 		Vud = 0;
-
 		#ifdef DEBUG_SENSORS
-		SendComChar(';');
-		SendComChar(';');
-		SendComChar(';');
+		Trace[TAx] = 0;
+		Trace[TAz] = 0;
+		Trace[TAy] = 0;	
 		#endif
 	}
+} // AccelerationCompensation
 
-} // CheckLISL
