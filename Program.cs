11b70b4bfb745b8d3bcb593424eaefdd307fe3e6
gke3500 <gke3500@6150ef54-a058-11dd-a159-232c7ab52622>
2010-04-19 06:20:41 +0000

    Minor cleanups.

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace UAVXGS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Altitude());
+            Application.Run(new FormMain());
         }
     }
 }
