2913a618ba26988d2d80965e05199fbe39503f65
gke3500 <gke3500@6150ef54-a058-11dd-a159-232c7ab52622>
2010-10-29 09:01:21 +0000

    GKE R678. Restored yaw scaling.

--- gyro_analog.h
+++ gyro_analog.h
@@ -69,12 +69,13 @@ void CalculateGyroRates(void)
 		// dual not used for Yaw 
 		break;
  	case Gyro300D3V:
-		break;
-	case Gyro300D5V:
 		YawRate = SRS16(YawRate, 1);
 		break;
-	case Gyro150D5V:
+	case Gyro300D5V:
 		YawRate = SRS16(YawRate, 2);
+		break;
+	case Gyro150D5V:
+		YawRate = SRS16(YawRate, 3);
 		break;
 	} // GyroYawType
 
